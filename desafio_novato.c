749de2813486f41cd2cf9b725333b78ff06df333
amandagabh <202501426026@alunos.estacio.br>
2025-05-05 20:28:15 -0300

    Add files via upload
    Desafio: nível novato
    Movimentando as Peças do xadrez

---
+++ desafio_novato.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+    int casasTorre = 5;
+
+    printf("Movimento da Torre:\n");
+
+  
+    for (int i = 1; i <= casasTorre; i++) {
+        printf("Direita\n");
+    }
+
+    int casasBispo = 5;
+    int contadorBispo = 1;
+
+    printf("\nMovimento do Bispo:\n");
+
+    while (contadorBispo <= casasBispo) {
+        printf("Cima Direita\n");
+        contadorBispo++;
+    }
+
+    int casasRainha = 8;
+    int contadorRainha = 1;
+
+    printf("\nMovimento da Rainha:\n");
+
+
+    do {
+        printf("Esquerda\n");
+        contadorRainha++;
+    } while (contadorRainha <= casasRainha);
+
+    return 0;
+}
